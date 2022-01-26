808844a1bd46e134b214c1443cc19bb770647aca
AbrQ <abrahamqt@gmail.com>
2022-01-25 20:07:18 -0600

    Program01 added into repository

---
+++ Program01.java
@@ -0,0 +1,28 @@
+//Program 01
+
+public class Program01{
+
+	public static void main(String args[]){ 
+
+		/*String name;
+		String last_name = "Quezada", full_name = "Abraham Quezada";
+
+		name = "Abraham";
+
+		System.out.println("This is my name: "+name);
+		System.out.println("This is my last name: "+last_name);
+		System.out.println("My full name is: "+full_name);*/
+
+		/*
+		int age = 27;
+		double pi = 3.1416;
+
+		System.out.println("Ten years ago, I was: "+(age - 10));
+		System.out.println("Finally, 3 years in the future I'll: "+(age + 3));
+		System.out.println("If I count Pi value two times, it is: "+(pi * 2));
+		System.out.println("1/2 Pi value is: "+(pi / 2));
+		*/
+
+	 }
+
+}
