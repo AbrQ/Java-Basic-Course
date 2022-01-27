5b9e07f0351963884fbcb97177008fcbac2ccfe4
AbrQ <abrahamqt@gmail.com>
2022-01-26 20:03:38 -0600

    Program02 added into repository

---
+++ User.java
@@ -0,0 +1,31 @@
+//Program02
+
+class User{
+	String name;
+	int age;
+	String address;
+
+	void setName(String var1){
+		name = var1;
+	}
+
+	String getName(){
+		return name;
+	}
+
+	void setAge(int var2){
+		age = var2;
+	}
+
+	int getAge(){
+		return age;
+	}
+
+	void setAddress(String var3){
+		address = var3;
+	}
+
+	String getAddress(){
+		return address;
+	}
+}
