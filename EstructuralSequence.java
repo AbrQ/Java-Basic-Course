64555a01a3d5846803383f27034d417c690b1706
AbrQ <abrahamqt@gmail.com>
2022-02-01 19:20:59 -0600

    Adding EstructuralSequence.java Program

---
+++ EstructuralSequence.java
@@ -0,0 +1,18 @@
+class EstructuralSequence{
+	public static void main(String args[]){
+
+		System.out.println("Step 1");
+		System.out.println("Step 2");
+		System.out.println("Step 3");
+		System.out.println("Step 4");
+		System.out.println("Step 5");
+		System.out.println("Step 6...");
+		System.out.println(" ^__^");
+		System.out.println(" (oo)\\_______");
+		System.out.println(" (__)\\       )\\/\\");
+		System.out.println("      ||____w |");
+		System.out.println("      ||     ||");
+		
+
+	}
+}
