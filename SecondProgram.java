1b640608c6ae5ea01aee80aa6aa078df8d4ced58
AbrQ <abrahamqt@gmail.com>
2022-02-05 11:01:59 -0600

    Adding SecondProgram into my repository

---
+++ SecondProgram.java
@@ -0,0 +1,19 @@
+class SecondProgram{
+	public static void main(String args[]){
+
+		String name = "Abraham", lastName = "Quezada";
+		String animal;
+		Character charact = 'A';
+		int age = 27;
+		String nickname = "abe";
+		double anyNumber = 27.5;
+
+
+		animal = "Lion";
+
+		System.out.println("My name is: "+name+" and my last name is: "+lastName);
+		System.out.println("My favorite animal is: "+animal);
+		System.out.println("Im :"+age+" and 3 years in future I will: "+(age + 3));
+
+	}
+}
